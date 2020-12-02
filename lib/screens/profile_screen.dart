f4ce6e58b9ac0d5857a01dec5ee42612a433d5c5
sannidhya-kushwaha <sannidhyakushwaha@gmail.com>
2020-12-02 13:57:55 +0530

    Finishing Our Profile Screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:Social_Media_App_UI/models/user_model.dart';
 import 'package:Social_Media_App_UI/widgets/custom_drawer.dart';
+import 'package:Social_Media_App_UI/widgets/posts_carousel.dart';
 import 'package:Social_Media_App_UI/widgets/profile_clipper.dart';
 import 'package:flutter/material.dart';
 
@@ -14,63 +15,141 @@ class ProfileScreen extends StatefulWidget {
 
 class _ProfileScreenState extends State<ProfileScreen> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  PageController _yourPostsPageController;
+  PageController _favoritesPageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _yourPostsPageController =
+        PageController(initialPage: 0, viewportFraction: 0.8);
+    _favoritesPageController =
+        PageController(initialPage: 0, viewportFraction: 0.8);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        key: _scaffoldKey,
-        drawer: CustomDrawer(),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Stack(
-                alignment: Alignment.center,
-                children: [
-                  ClipPath(
-                    clipper: ProfileClipper(),
-                    child: Image(
-                      height: 300.0,
-                      width: double.infinity,
-                      image: AssetImage(widget.user.backgroundImageUrl),
-                      fit: BoxFit.cover,
-                    ),
+      key: _scaffoldKey,
+      drawer: CustomDrawer(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                ClipPath(
+                  clipper: ProfileClipper(),
+                  child: Image(
+                    height: 300.0,
+                    width: double.infinity,
+                    image: AssetImage(widget.user.backgroundImageUrl),
+                    fit: BoxFit.cover,
                   ),
-                  Positioned(
-                    top: 50.0,
-                    left: 20.0,
-                    child: IconButton(
-                      icon: Icon(Icons.menu),
-                      iconSize: 30.0,
-                      color: Theme.of(context).primaryColor,
-                      onPressed: () => _scaffoldKey.currentState.openDrawer(),
-                    ),
+                ),
+                Positioned(
+                  top: 50.0,
+                  left: 20.0,
+                  child: IconButton(
+                    icon: Icon(Icons.menu),
+                    iconSize: 30.0,
+                    color: Theme.of(context).primaryColor,
+                    onPressed: () => _scaffoldKey.currentState.openDrawer(),
                   ),
-                  Positioned(
-                    bottom: 10.0,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black45,
-                            offset: Offset(0, 2),
-                            blurRadius: 6.0,
-                          ),
-                        ],
-                      ),
-                      child: ClipOval(
-                        child: Image(
-                          height: 120.0,
-                          width: 120.0,
-                          image: AssetImage(widget.user.profileImageUrl),
-                          fit: BoxFit.cover,
+                ),
+                Positioned(
+                  bottom: 10.0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black45,
+                          offset: Offset(0, 2),
+                          blurRadius: 6.0,
                         ),
+                      ],
+                    ),
+                    child: ClipOval(
+                      child: Image(
+                        height: 120.0,
+                        width: 120.0,
+                        image: AssetImage(widget.user.profileImageUrl),
+                        fit: BoxFit.cover,
                       ),
                     ),
                   ),
-                ],
-              )
-            ],
-          ),
-        ));
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Text(
+                widget.user.name,
+                style: TextStyle(
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.5,
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      'Following',
+                      style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                    SizedBox(height: 2.0),
+                    Text(
+                      widget.user.following.toString(),
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      'Followers',
+                      style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                    SizedBox(height: 2.0),
+                    Text(
+                      widget.user.followers.toString(),
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            PostsCarousel(
+              pageController: _yourPostsPageController,
+              title: 'Your Posts',
+              posts: widget.user.posts,
+            ),
+            PostsCarousel(
+              pageController: _favoritesPageController,
+              title: 'Favorites',
+              posts: widget.user.posts,
+            ),
+            SizedBox(height: 50.0),
+          ],
+        ),
+      ),
+    );
   }
 }
