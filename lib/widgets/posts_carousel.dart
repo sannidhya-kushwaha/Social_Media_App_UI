64055cc65be1501f495abb8b11694fd1cc5bf9ce
sannidhya-kushwaha <sannidhyakushwaha@gmail.com>
2020-12-02 12:22:02 +0530

    Positioning container

--- lib/widgets/posts_carousel.dart
+++ lib/widgets/posts_carousel.dart
@@ -51,9 +51,9 @@ class PostsCarousel extends StatelessWidget {
             ),
           ),
           Positioned(
-            left: 0.0,
-            bottom: 0.0,
-            right: 0.0,
+            left: 10.0,
+            bottom: 10.0,
+            right: 10.0,
             child: Container(
               padding: EdgeInsets.all(12.0),
               height: 110.0,
